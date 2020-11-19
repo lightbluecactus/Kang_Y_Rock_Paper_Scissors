acef5b9db709ee53fe66a4122eaa918b08ba946a
lightbluecactus <violetday77@yahoo.com>
2020-11-19 18:13:22 -0400

    update the loop

--- game.py
+++ game.py
@@ -7,45 +7,70 @@ from random import randint
 # this is all of the three options.
 choices = ["rock", "paper", "scissors"]
 
-# computer is our AI opponent - let it make a choice
-# generate a random number, and the number reflects the three options.
-computer = choices[randint(0,2)]
+# give our player and AI some lives
+player_lives = 5
+ai_lives = 5
 
-# give the user some weapon options
-# input("Pick rock, paper, or scissors: ") is a prompt
-# my_choice = is creating a variable
-player = input("Pick rock, paper, or scissors: ")
+# true and false are boolean data types -> the equivalent of 1 and 0
+player = False
 
-# valdate that the input worked 
-# print means showing the result
-print("Player chose " + player)
-print("AI chose: " + computer)
+# remember to make everything under indence
+# same as while player == False
+while player is False:
 
-# check to see who won or if it's a tie
-if (computer == player):
-	print("tie")
-	#reset the game loop and have the user choose again
+	# this is the player choice
+	# input("Pick rock, paper, or scissors: ") is a prompt
+	# my_choice = is creating a variable
+	player = input("Pick rock, paper, or scissors: ")
 
-elif (computer == "rock"):
-	if (player == "scissors"):
-		print("you lose!")
-		# take a life away from the player
-	else:
-		print("you won!")
-		# take a life away from the player
+	# player has a value, so it's true now.
 
-elif (computer == "paper"):
-	if (player == "rock"):
-		print("you lose!")
-		# take a life away from the player
-	else:
-		print("you won!")
-		# take a life away from the player
+	# this will be the AI choice
+	# generate a random number, and the number reflects the three options.
+	computer = choices[randint(0,2)]
 
-elif (computer == "scissors"):
-	if (player == "paper"):
-		print("you lose!")
-		# take a life away from the player
-	else:
-		print("you won!")
-		# take a life away from the player
+	# valdate that the input worked 
+	# print means showing the result
+	print("Player chose " + player)
+	#validate that the random choice worked for the ai
+	print("AI chose: " + computer)
+
+	# check to see who won or if it's a tie
+	if (computer == player):
+		print("tie")
+		# always check for negative conditions first (the losing case)
+
+	elif (computer == "rock"):
+		if (player == "scissors"):
+			print("you lose!")
+			# take a life away from the player
+			player_lives = player_lives - 1 
+
+			# could also be player_lives -= 1 for short
+
+		else:
+			print("you won!")
+			# take a life away from the ai
+			ai_lives = ai_lives - 1
+
+	elif (computer == "paper"):
+		if (player == "rock"):
+			print("you lose!")
+			player_lives = player_lives - 1
+		else:
+			print("you won!")
+			ai_lives = ai_lives - 1
+
+	elif (computer == "scissors"):
+		if (player == "paper"):
+			print("you lose!")
+			player_lives = player_lives - 1
+		else:
+			print("you won!")
+			ai_lives = ai_lives - 1
+
+	print("player has", player_lives, "lives left")
+	print("AI has", ai_lives, "lives left")
+
+	# set player back to false to keep it running in loop
+	player = False
