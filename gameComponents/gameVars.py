9f9fbd7432ecb9e46cbbea005f913394a9c99160
lightbluecactus <violetday77@yahoo.com>
2020-11-26 18:28:23 -0400

    adding packages

---
+++ gameComponents/gameVars.py
@@ -0,0 +1,13 @@
+# [] => an array is a datatype in nerd-speak ==> arrays are 0-based
+# every entry gets an index
+# this is all of the three options.
+choices = ["rock", "paper", "scissors"]
+
+# give our player and AI some lives
+player_lives = 5
+ai_lives = 5
+
+total_lives = 5
+
+# true and false are boolean data types -> the equivalent of 1 and 0
+player = False
